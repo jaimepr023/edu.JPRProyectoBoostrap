1a85bf481c6542b0d3de9d24115a2679f04cf8eb
Jaime Prieto <jaime.p.r023@gmail.com>
2024-05-03 13:45:19 +0200

    formulario hecho

--- src/main/webapp/javaScript/Js.js
+++ src/main/webapp/javaScript/Js.js
@@ -2,8 +2,28 @@
  * 
  */
 
- function verificarContrasenia(){
-	 let contrasena1 = document.getElementById("exampleInputPassword1").value;
-	 console.log(contrasena1);
-	 
- }
+var BDDProductos = [];
+
+function verificarContrasenia() {
+	let contrasena1 = document.getElementById("exampleInputPassword1").value;
+	let contrasena2 = document.getElementById("exampleInputPassword2").value;
+	if (contrasena1 === contrasena2) {
+		return true;
+	} else {
+		alert("Las contraseñas no son iguales, por favor intentelo de nuevo");
+		document.getElementById("exampleInputPassword2").focus();
+		return false;
+	}
+}
+
+function verificarContrasenia1() {
+	let contrasena3 = document.getElementById("exampleInputPassword3").value;
+	let contrasena4 = document.getElementById("exampleInputPassword4").value;
+if (contrasena3 === contrasena4) {
+		return true;
+	} else {
+		alert("Las contraseñas no son iguales, por favor intentelo de nuevo");
+		document.getElementById("exampleInputPassword4").focus();
+		return false;
+	}
+}
