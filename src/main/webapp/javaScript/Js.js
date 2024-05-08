c314f2aab2bf465d53fa3d76ab2210e0e351c349
Jaime Prieto <jaime.p.r023@gmail.com>
2024-05-08 10:11:29 +0200

    ArrayDeMotos

--- src/main/webapp/javaScript/Js.js
+++ src/main/webapp/javaScript/Js.js
@@ -27,3 +27,5 @@ if (contrasena3 === contrasena4) {
 		return false;
 	}
 }
+
+const todasLasMotos = [[4500,"KTM Duke 125",0],[7000,"Aprilia RS 475", 0],[30000,"Yamaha YZF-R1", 0],[13200,"Yamaha R6 Race",0],[15500,"Honda VT 1300 CX FURY",0],[10500,"Honda CRF450R",0],[4000,"Kymco Super Dink",0],[4700,"Primavera 125 Euro 5",0]];
